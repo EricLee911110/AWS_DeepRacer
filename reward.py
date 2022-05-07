22f531fd52ce1f9938bb82dedb419971aa7a1692
Eric Lee <ericlee911110@gmail.com>
2022-05-07 15:45:06 +0800

    Second try on reward function

--- reward.py
+++ reward.py
@@ -1,31 +1,52 @@
+import math
+
 def reward_function(params):
     # Example of penalize steering, which helps mitigate zig-zag behaviors
 
     # Read input parameters
     distance_from_center = params['distance_from_center']
     track_width = params['track_width']
-    abs_steering = abs(params['steering_angle']) # Only need the absolute steering angle
+    waypoints = params['waypoints']
+    closest_waypoints = params['closest_waypoints']
+    heading = params['heading']
+    all_wheels_on_track = params['all_wheels_on_track']
+    progress = params['progress']
 
-    # Calculate 3 marks that are farther and father away from the center line
+    # Center Line
     marker_1 = 0.12 * track_width
-    marker_2 = 0.25 * track_width
+    marker_2 = 0.3 * track_width
     marker_3 = 0.5 * track_width
 
-    # Give higher reward if the car is closer to center line and vice versa
     if distance_from_center <= marker_1:
         reward = 1.0
     elif distance_from_center <= marker_2:
-        reward = 0.7
+        reward = 0.9
     elif distance_from_center <= marker_3:
-        reward = 0.3
+        reward = 0.1
     else:
         reward = 1e-3  # likely crashed/ close to off track
 
-    # Steering penality threshold, change the number based on your action space setting
-    ABS_STEERING_THRESHOLD = 15 
+    # Angle
+    next_WP = waypoints[closest_waypoints[1]]
+    prev_WP = waypoints[closest_waypoints[0]]
+
+    track_angle = math.degrees(math.atan2(next_WP[1] - prev_WP[1], next_WP[0] - prev_WP[0]))
+    angle_diff = abs(heading - track_angle)
+
+    if angle_diff > 180:
+        angle_diff = abs(360 - angle_diff)
+
+    ANGLE_THRESHOLD = 20
+
+    if angle_diff > ANGLE_THRESHOLD:
+        reward *= 0.1
+
+    # All wheels on track
+    if all_wheels_on_track == False:
+        reward *= 0.01
     
-    # Penalize reward if the car is steering too much
-    if abs_steering > ABS_STEERING_THRESHOLD:
-        reward *= 0.8
-        
+
+    
+
+
     return float(reward)
