db5836ebb24cef5545ca7155005ffd54a736a96c
Eric Lee <ericlee911110@gmail.com>
2022-05-08 12:26:28 +0800

    heading toward next waypoint strategy.

--- reward.py
+++ reward.py
@@ -11,6 +11,9 @@ def reward_function(params):
     heading = params['heading']
     all_wheels_on_track = params['all_wheels_on_track']
     progress = params['progress']
+    steering_angle = params['steering_angle']
+    x = params['x']
+    y = params['y']
 
     # Center Line
     marker_1 = 0.12 * track_width
@@ -27,26 +30,23 @@ def reward_function(params):
         reward = 1e-3  # likely crashed/ close to off track
 
     # Angle
-    next_WP = waypoints[closest_waypoints[1]]
-    prev_WP = waypoints[closest_waypoints[0]]
+    next_wp = waypoints[closest_waypoints[1]]
+    prev_wp = waypoints[closest_waypoints[0]]
 
-    track_angle = math.degrees(math.atan2(next_WP[1] - prev_WP[1], next_WP[0] - prev_WP[0]))
-    angle_diff = abs(heading - track_angle)
+    car_next_wp_angle = math.degrees(math.atan2(next_wp[1] - y, next_wp[0] - x))
+    angle_diff = abs(heading - car_next_wp_angle)
 
     if angle_diff > 180:
         angle_diff = abs(360 - angle_diff)
 
-    ANGLE_THRESHOLD = 20
+    ANGLE_THRESHOLD = 40
 
     if angle_diff > ANGLE_THRESHOLD:
-        reward *= 0.1
+        reward *= 0.01
 
     # All wheels on track
     if all_wheels_on_track == False:
         reward *= 0.01
-    
-
-    
 
 
     return float(reward)
