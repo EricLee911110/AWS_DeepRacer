88cb294e23a3a3140474090004834cc8d90d120a
Eric Lee <ericlee911110@gmail.com>
2022-05-10 18:31:24 +0800

    29th Attempt. Final update on May, 2022

--- reward.py
+++ reward.py
@@ -1,71 +1,85 @@
 import math
 
+class Reward():
+    def __init__(self):
+        self.prev_speed = 0
+        self.speed_list = []
+        self.prev_progress = 0
+        self.prev_progress_diff = 0
+
+    def reward_function_in_class(self, params):
+        # Read input parameters
+        distance_from_center = params['distance_from_center']
+        track_width = params['track_width']
+        waypoints = params['waypoints']
+        closest_waypoints = params['closest_waypoints']
+        heading = params['heading']
+        all_wheels_on_track = params['all_wheels_on_track']
+        progress = params['progress']
+        steering_angle = params['steering_angle']
+        x = params['x']
+        y = params['y']
+        steps = params['steps']
+        is_left_of_center = params['is_left_of_center']
+        speed = params['speed']
+
+        # Center Line
+        reward = 1
+
+        marker_1 = 0.12 * track_width
+        marker_2 = 0.3 * track_width
+        marker_3 = 0.4 * track_width
+        marker_4 = 0.5 * track_width
+
+        # Right-turn corner of Left-turn Corner
+        next_wp = waypoints[closest_waypoints[1]]
+        prev_wp = waypoints[closest_waypoints[0]]
+
+        if closest_waypoints[1] == len(waypoints) -1:
+            n_next_wp = waypoints[0]
+        else:
+            n_next_wp = waypoints[closest_waypoints[1] +1]
+
+        angle_now = math.degrees(math.atan2(next_wp[1] - prev_wp[1], next_wp[0] - prev_wp[0]))
+        angle_next = math.degrees(math.atan2(n_next_wp[1] - next_wp[1], n_next_wp[0] - next_wp[0]))
+
+        if angle_next - angle_now > 0:     #Trun left
+            if is_left_of_center == False:
+                reward *= 0.6
+            else:
+                reward *= 1.02
+            if distance_from_center > marker_1 and distance_from_center <= marker_4:
+                reward *= 1.21
+            else:
+                reward *= 0.6
+
+        else:   #straight line or turn right
+            if is_left_of_center == False:
+                reward *= 1.01
+            if distance_from_center <= marker_1:
+                reward *= 1.25
+            elif distance_from_center <= marker_2:
+                reward *= 1.2
+            elif distance_from_center <= marker_3:
+                reward *= 0.8
+            elif distance_from_center <= marker_4:
+                reward *= 0.6
+            else:
+                reward = 1e-3  
+
+        # Speeding up by average speed
+        self.speed_list.append(speed)
+        if steps % 15 == 0:    # check every 1 seconds
+            avg_speed = sum(self.speed_list) / 15
+            if avg_speed > self.prev_speed:
+                reward *= 1.22
+            self.prev_speed = avg_speed
+            self.speed_list = []
+
+        return float(reward)
+
+
+reward_object = Reward()
 
 def reward_function(params):
-    # Example of penalize steering, which helps mitigate zig-zag behaviors
-
-    # Read input parameters
-    distance_from_center = params['distance_from_center']
-    track_width = params['track_width']
-    waypoints = params['waypoints']
-    closest_waypoints = params['closest_waypoints']
-    heading = params['heading']
-    all_wheels_on_track = params['all_wheels_on_track']
-    progress = params['progress']
-    steering_angle = params['steering_angle']
-    x = params['x']
-    y = params['y']
-    steps = params['steps']
-    is_left_of_center = params['is_left_of_center']
-
-    # Center Line
-    marker_1 = 0.12 * track_width
-    marker_2 = 0.3 * track_width
-    marker_3 = 0.5 * track_width
-
-    if distance_from_center <= marker_1:
-        reward = 1.0
-    elif distance_from_center <= marker_2:
-        reward = 0.9
-    elif distance_from_center <= marker_3:
-        reward = 0.1
-    else:
-        reward = 1e-3  # likely crashed/ close to off track
-
-    # Stay on right side of track
-    if is_left_of_center == False:
-        reward *= 1.1
-
-    # Right-turn corner of Left-turn Corner
-    next_wp = waypoints[closest_waypoints[1]]
-    prev_wp = waypoints[closest_waypoints[0]]
-
-    if closest_waypoints[1] == len(waypoints) -1:
-        n_next_wp = waypoints[1]
-    else:
-        n_next_wp = waypoints[closest_waypoints[1] +1]
-
-    angle_now = math.degrees(math.atan2(next_wp[1] - prev_wp[1], next_wp[0] - prev_wp[0]))
-    angle_next = math.degrees(math.atan2(n_next_wp[1] - next_wp[1], n_next_wp[0] - next_wp[0]))
-
-    if angle_next - angle_now > 10:     #Left turn
-        if is_left_of_center == False:
-            reward *= 0.7
-    if angle_next - angle_now < -10:    #Right turn
-        if is_left_of_center == True:
-            reward *= 0.7
-
-    # Speeding up by progress
-    if steps % 450 == 0:    # check per 30 seconds
-        if steps == 0:
-            prev_progress = 0
-            prev_progress_diff = 0
-        progress_diff = progress - prev_progress
-        prev_progress = progress
-        
-        if progress_diff > prev_progress_diff:
-            reward *= 1.1
-        prev_progress_diff = progress_diff
-
-
-    return float(reward)
+    return reward_object.reward_function_in_class(params)
--- test.py
+++ test.py
@@ -1,3 +1,31 @@
-a = 1
-a *= 1.2 
-print(a)
+class Reward:
+    def __init__(self):
+        self.prev_speed = 0
+        self.speed_list = [0]
+
+    def reward_function_in_class(self, params):
+        score = 0
+        
+        speed = params['speed']
+
+        if self.prev_speed < speed:
+            score += 1
+            self.prev_speed = speed
+            print(self.prev_speed)
+        self.speed_list.append(speed)
+        print(self.speed_list)
+        print(sum(self.speed_list))
+        self.speed_list = []
+        
+        return score
+
+
+reward_object = Reward()
+
+def reward_function(params):
+    return reward_object.reward_function_in_class(params)
+
+reward_function({'speed':100})
+reward_function({'speed':200})
+reward_function({'speed':300})
+reward_function({'speed':400})
